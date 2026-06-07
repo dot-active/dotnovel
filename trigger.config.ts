3b163fd383805d8e6167b4724423f15519effc0d
dot-active <rudtriplel@gmail.com>
2026-06-07 22:07:21 +1000

    fixing  trigger

--- trigger.config.ts
+++ trigger.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from '@trigger.dev/sdk/v3'
+import { prismaExtension } from '@trigger.dev/build/extensions/prisma'
 
 export default defineConfig({
   project: 'proj_dotnovel',
@@ -6,4 +7,12 @@ export default defineConfig({
   logLevel: 'log',
   maxDuration: 3600,
   dirs: ['./src/trigger'],
+  build: {
+    extensions: [
+      prismaExtension({
+        mode: 'legacy',
+        schema: 'prisma/schema.prisma',
+      }),
+    ],
+  },
 })
